3878c6b5b2c0b0021134125041cd920b7fb848ac
George Mokbel <georgemokbel@gmail.com>
2016-03-12 08:58:12 -0800

    Add WebApiConfig, remove unused usings.

--- LeanOwinApi/Startup.cs
+++ LeanOwinApi/Startup.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Http;
+﻿using System.Web.Http;
 using Microsoft.Owin.Cors;
 using Owin;
 using Topshelf;
@@ -21,7 +16,7 @@ namespace LeanOwinApi
         {
             var httpConfiguration = new HttpConfiguration();
 
-
+            WebApiConfig.Configure(httpConfiguration);
 
             app.UseWebApi(httpConfiguration);
             app.UseCors(CorsOptions.AllowAll);
---
+++ LeanOwinApi/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace LeanOwinApi
+{
+    internal sealed class WebApiConfig
+    {
+        public static void Configure(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            RemoveXmlFormatter(config);
+
+            ConfigureJsonSerializerSettings(config);
+        }
+
+        private static void ConfigureJsonSerializerSettings(HttpConfiguration config)
+        {
+            var serializerSettings = config.Formatters.JsonFormatter.SerializerSettings ?? new JsonSerializerSettings();
+            serializerSettings.Converters.Add(new IsoDateTimeConverter { DateTimeFormat = "yyyy-MM-ddTHH:mm:ssZ" });
+            serializerSettings.Converters.Add(new StringEnumConverter());
+            config.Formatters.JsonFormatter.SerializerSettings = serializerSettings;
+        }
+
+        private static void RemoveXmlFormatter(HttpConfiguration config)
+        {
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+        }
+    }
+}
