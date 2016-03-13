d0d04a20b36d3b33b8e5286cd00ddd8158a096b6
George Mokbel <georgemokbel@gmail.com>
2016-03-13 07:46:52 -0700

    Add IConfigurationService interface.

---
+++ LeanOwinApi/Services/IConfigurationService.cs
@@ -0,0 +1,9 @@
+﻿namespace LeanOwinApi.Services
+{
+    public interface IConfigurationService
+    {
+        string GetString(string key);
+        bool GetBool(string key);
+        int GetInt(string key);
+    }
+}
